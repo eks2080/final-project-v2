717cb436200e50159a765558c18464dd19bbed56
eks2080 <eks2080@nyu.edu>
2024-05-09 14:55:44 -0400

    Map is functioning, but struggling with get markers to appear

--- js/scripts.js
+++ js/scripts.js
@@ -59,57 +59,57 @@ grocerystores.forEach(function (grocerystoresRecord) {
         .addTo(map);
 })
 
-/* map.on('load', function () {
+map.on('load', function () {
 
-/*     // add a geojson source for the borough boundaries
-    map.addSource('borough-boundaries', {
-        type: 'geojson',
-        data: 'data/borough-boundaries-simplified.geojson',
-        generateId: true // this will add an id to each feature, this is necessary if we want to use featureState (see below)
-    }) */
+    /*     // add a geojson source for the borough boundaries
+        map.addSource('borough-boundaries', {
+            type: 'geojson',
+            data: 'data/borough-boundaries-simplified.geojson',
+            generateId: true // this will add an id to each feature, this is necessary if we want to use featureState (see below)
+        }) */
 
     // not sure if using this yet; first add the fill layer, using a match expression to give each a unique color based on its boro_code property
-//     map.addLayer({
-//         id: 'borough-boundaries-fill',
-//         type: 'fill',
-//         source: 'borough-boundaries',
-//         paint: {
-//             'fill-color': [
-//                 'match',
-//                 ['get', 'boro_code'],
-//                 '1',
-//                 '#f4cae4',
-//                 '2',
-//                 '#cbd5e8',
-//                 '3',
-//                 '#fdcdac',
-//                 '4',
-//                 '#b3e2cd',
-//                 /*                 '5',
-//                                 '#e6f5c9', */
-//                 '#ccc'
-//             ],
-//             // use a case expression to set the opacity of a polygon based on featureState
-//             'fill-opacity': [
-//                 'case',
-//                 ['boolean', ['feature-state', 'hover'], false],
-//                 1,  // opacity when hover is false
-//                 0.5 // opacity when hover is true
-//             ]
-//         }
-//     })
-//     // add borough outlines after the fill layer, so the outline is "on top" of the fill
-//     map.addLayer({
-//         id: 'borough-boundaries-line',
-//         type: 'line',
-//         source: 'borough-boundaries',
-//         paint: {
-//             'line-color': '#6b6b6b',
-//         }
-//     })
+    //     map.addLayer({
+    //         id: 'borough-boundaries-fill',
+    //         type: 'fill',
+    //         source: 'borough-boundaries',
+    //         paint: {
+    //             'fill-color': [
+    //                 'match',
+    //                 ['get', 'boro_code'],
+    //                 '1',
+    //                 '#f4cae4',
+    //                 '2',
+    //                 '#cbd5e8',
+    //                 '3',
+    //                 '#fdcdac',
+    //                 '4',
+    //                 '#b3e2cd',
+    //                 /*                 '5',
+    //                                 '#e6f5c9', */
+    //                 '#ccc'
+    //             ],
+    //             // use a case expression to set the opacity of a polygon based on featureState
+    //             'fill-opacity': [
+    //                 'case',
+    //                 ['boolean', ['feature-state', 'hover'], false],
+    //                 1,  // opacity when hover is false
+    //                 0.5 // opacity when hover is true
+    //             ]
+    //         }
+    //     })
+    //     // add borough outlines after the fill layer, so the outline is "on top" of the fill
+    //     map.addLayer({
+    //         id: 'borough-boundaries-line',
+    //         type: 'line',
+    //         source: 'borough-boundaries',
+    //         paint: {
+    //             'line-color': '#6b6b6b',
+    //         }
+})
 
 // click on a button and use flyTo to go to that borough. needs to be adjusted, re assignment 4 notes
-/* $('#queens-button').on('click', function () {
+$('#queens-button').on('click', function () {
     map.flyTo({
         center: [-73.89387763569168, 40.73104567408716],
         zoom: 11,
@@ -145,4 +145,4 @@ $('#staten-island-button').on('click', function () {
         zoom: 11,
         duration: 1500
     })
-}) */
+}) 
